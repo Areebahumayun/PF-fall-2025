07f591cfcc11e23383afa47d4971fb127b28aa3c
Areebahumayun <areebahmn@gmail.com>
2025-10-16 12:53:32 -0700

    Add question 2 solution

---
+++ question2_solution.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int i,fact=1,num;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	for(int i=1;i<=num;i++){
+	
+		fact=fact*i;}
+	printf("the factorial is %d",fact);
+}
