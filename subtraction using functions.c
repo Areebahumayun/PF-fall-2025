043050a73bb04deb2c296e11147ff823fe91689b
Areeba humayun <areebahmn@gmail.com>
2025-11-15 13:45:29 +0500

    Add files via upload

---
+++ subtraction using functions.c
@@ -0,0 +1,31 @@
+
+
+#include<stdio.h>
+int subtraction(int a,int b ){
+	return a-b;
+}
+
+
+int main(){
+	int a,b;
+	printf("enter the value of a");
+	scanf("%d",&a);
+	printf("enter the value of b");
+	scanf("%d",&b);
+	int sub=subtraction(a,b);
+	printf("%d",sub);
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
