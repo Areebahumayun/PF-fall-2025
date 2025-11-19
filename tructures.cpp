a9853e8bd67a9b38697ca7630f9e26ddcc8e803b
Areeba humayun <areebahmn@gmail.com>
2025-11-19 22:03:39 +0500

    Add files via upload

---
+++ tructures.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+	struct student{
+		int number;
+		char name[20];
+		char adress[40];
+		
+	};
+int main(){
+struct student s1;
+printf("enter the name");
+scanf("%s",s1.name);
+printf("enter the number");
+scanf("%d",&s1.number);
+printf("enter the adress");
+scanf("%s",s1.adress);
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
